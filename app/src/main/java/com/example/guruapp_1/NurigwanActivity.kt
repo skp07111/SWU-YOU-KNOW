c38c6c66d3f25e7309e2af7a0f93bb2ae40b8269
skp07111 <skp07111@naver.com>
2022-07-25 02:14:44 +0900

    다른 가게들 입력

--- app/src/main/java/com/example/guruapp_1/NurigwanActivity.kt
+++ app/src/main/java/com/example/guruapp_1/NurigwanActivity.kt
@@ -5,24 +5,24 @@ import android.os.Bundle
 import android.view.ContextMenu
 import android.view.MenuItem
 import android.view.View
-import kotlinx.android.synthetic.main.nurigwan.*
+import kotlinx.android.synthetic.main.activity_nurigwan.*
 
 class NurigwanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.nurigwan)
-        registerForContextMenu(button1)
-        registerForContextMenu(button2)
-        registerForContextMenu(button3)
-        registerForContextMenu(button4)
-        registerForContextMenu(button5)
-        registerForContextMenu(button6)
-        registerForContextMenu(button7)
-        registerForContextMenu(button8)
-        registerForContextMenu(button9)
-        registerForContextMenu(button12)
-        registerForContextMenu(button13)
-        registerForContextMenu(button14)
+        setContentView(R.layout.activity_nurigwan)
+        registerForContextMenu(nuri_button1)
+        registerForContextMenu(nuri_button2)
+        registerForContextMenu(nuri_button3)
+        registerForContextMenu(nuri_button4)
+        registerForContextMenu(nuri_button5)
+        registerForContextMenu(nuri_button6)
+        registerForContextMenu(nuri_button7)
+        registerForContextMenu(nuri_button8)
+        registerForContextMenu(nuri_button9)
+        registerForContextMenu(nuri_button12)
+        registerForContextMenu(nuri_button13)
+        registerForContextMenu(nuri_button14)
     }
 
     override fun onCreateContextMenu(
@@ -33,51 +33,51 @@ class NurigwanActivity : AppCompatActivity() {
         super.onCreateContextMenu(menu, v, menuInfo)
 
         when (v?.id) {
-            R.id.button1 -> {
+            R.id.nuri_button1 -> {
                 menu?.setHeaderTitle("비틀쥬스 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_beetlejuice, menu)
             }
-            R.id.button2 -> {
+            R.id.nuri_button2 -> {
                 menu?.setHeaderTitle("퀴즈노스 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_quiznos, menu)
             }
-            R.id.button3 -> {
+            R.id.nuri_button3 -> {
                 menu?.setHeaderTitle("가은 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_gaeun, menu)
             }
-            R.id.button4 -> {
+            R.id.nuri_button4 -> {
                 menu?.setHeaderTitle("츄밥 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_choobap, menu)
             }
-            R.id.button5 -> {
+            R.id.nuri_button5 -> {
                 menu?.setHeaderTitle("우리은행 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_wooribank, menu)
             }
-            R.id.button6 -> {
+            R.id.nuri_button6 -> {
                 menu?.setHeaderTitle("누리스토어 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_nuristore, menu)
             }
-            R.id.button7 -> {
+            R.id.nuri_button7 -> {
                 menu?.setHeaderTitle("안경점 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_optician, menu)
             }
-            R.id.button8 -> {
+            R.id.nuri_button8 -> {
                 menu?.setHeaderTitle("Sweet U 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_sweetu, menu)
             }
-            R.id.button9 -> {
+            R.id.nuri_button9 -> {
                 menu?.setHeaderTitle("우체국 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_postoffice, menu)
             }
-            R.id.button12 -> {
+            R.id.nuri_button12 -> {
                 menu?.setHeaderTitle("만권화밥 혼잡도")
                 menuInflater.inflate(R.menu.popup_menu_mangwon, menu)
             }
-            R.id.button13 -> {
+            R.id.nuri_button13 -> {
             menu?.setHeaderTitle("최고당돈가스 혼잡도")
             menuInflater.inflate(R.menu.popup_menu_choigodang, menu)
             }
-            R.id.button14 -> {
+            R.id.nuri_button14 -> {
             menu?.setHeaderTitle("에땅 혼잡도")
             menuInflater.inflate(R.menu.popup_menu_etang, menu)
             }
@@ -86,42 +86,42 @@ class NurigwanActivity : AppCompatActivity() {
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
-            R.id.select1_beetlejuice -> button1.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_beetlejuice -> button1.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_beetlejuice -> button1.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_quiznos -> button2.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_quiznos -> button2.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_quiznos -> button2.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_gaeun -> button3.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_gaeun -> button3.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_gaeun -> button3.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_choobap -> button4.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_choobap-> button4.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_choobap -> button4.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_wooribank -> button5.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_wooribank -> button5.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_wooribank -> button5.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_nuristore -> button6.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_nuristore -> button6.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_nuristore -> button6.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_optician -> button7.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_optician -> button7.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_optician -> button7.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_sweetu -> button8.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_sweetu -> button8.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_sweetu -> button8.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_postoffice -> button9.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_postoffice -> button9.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_postoffice -> button9.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_mangwon -> button12.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_mangwon -> button12.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_mangwon -> button12.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_choigodang -> button13.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_choigodang -> button13.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_choigodang -> button13.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_etang -> button14.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_etang -> button14.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_etang -> button14.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_beetlejuice -> nuri_button1.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_beetlejuice -> nuri_button1.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_beetlejuice -> nuri_button1.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_quiznos -> nuri_button2.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_quiznos -> nuri_button2.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_quiznos -> nuri_button2.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_gaeun -> nuri_button3.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_gaeun -> nuri_button3.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_gaeun -> nuri_button3.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_choobap -> nuri_button4.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_choobap-> nuri_button4.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_choobap -> nuri_button4.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_wooribank -> nuri_button5.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_wooribank -> nuri_button5.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_wooribank -> nuri_button5.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_nuristore -> nuri_button6.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_nuristore -> nuri_button6.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_nuristore -> nuri_button6.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_optician -> nuri_button7.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_optician -> nuri_button7.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_optician -> nuri_button7.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_sweetu -> nuri_button8.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_sweetu -> nuri_button8.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_sweetu -> nuri_button8.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_postoffice -> nuri_button9.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_postoffice -> nuri_button9.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_postoffice -> nuri_button9.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_mangwon -> nuri_button12.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_mangwon -> nuri_button12.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_mangwon -> nuri_button12.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_choigodang -> nuri_button13.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_choigodang -> nuri_button13.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_choigodang -> nuri_button13.setBackgroundResource(R.drawable.button_background3)
+            R.id.select1_etang -> nuri_button14.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_etang -> nuri_button14.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_etang -> nuri_button14.setBackgroundResource(R.drawable.button_background3)
         }
         return super.onContextItemSelected(item)
     }
---
+++ app/src/main/java/com/example/guruapp_1/ShalomActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guruapp_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShalomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shalom)
+    }
+}
