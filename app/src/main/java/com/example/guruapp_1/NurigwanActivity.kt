0b1416f77fee0396ee651beb52fe3dd011726919
skp07111 <skp07111@naver.com>
2022-07-24 19:49:16 +0900

    혼잡도 구현 완료(long click)

--- app/src/main/java/com/example/guruapp_1/NurigwanActivity.kt
+++ app/src/main/java/com/example/guruapp_1/NurigwanActivity.kt
@@ -3,6 +3,9 @@ package com.example.guruapp_1
 import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.Toast
@@ -13,29 +16,30 @@ class NurigwanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nurigwan)
+        registerForContextMenu(button1)
+    }
 
-        var button1 = findViewById<Button>(R.id.button1)
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
 
-        button1.setOnClickListener {
-            var popupMenu = PopupMenu(applicationContext, it)
-
-            menuInflater?.inflate(R.menu.popup_menu, popupMenu.menu)
-            popupMenu.show()
-            popupMenu.setOnMenuItemClickListener {
-                when(it.itemId){
-                    R.id.select1 -> {
-                        return@setOnMenuItemClickListener true
-                    }
-                    R.id.select2 -> {
-                        return@setOnMenuItemClickListener true
-                    }
-                    R.id.select3 -> {
-                        return@setOnMenuItemClickListener true
-                    } else -> {
-                        return@setOnMenuItemClickListener false
-                    }
-                }
+        when (v?.id) {
+            R.id.button1 -> {
+                menu?.setHeaderTitle("혼잡도")
+                menuInflater.inflate(R.menu.popup_menu, menu)
             }
         }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.select1 -> button1.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2 -> button1.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3 -> button1.setBackgroundResource(R.drawable.button_background3)
+        }
+        return super.onContextItemSelected(item)
     }
 }
