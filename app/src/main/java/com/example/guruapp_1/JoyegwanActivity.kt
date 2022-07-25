f9aac708d28e3dc0de3e10fdb9885052932832df
skp07111 <skp07111@naver.com>
2022-07-25 18:35:14 +0900

    test

--- app/src/main/java/com/example/guruapp_1/JoyegwanActivity.kt
+++ app/src/main/java/com/example/guruapp_1/JoyegwanActivity.kt
@@ -2,10 +2,43 @@ package com.example.guruapp_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_joyegwan.*
 
 class JoyegwanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joyegwan)
+        registerForContextMenu(joye_button1)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        when (v?.id) {
+            R.id.joye_button1 -> {
+                menu?.setHeaderTitle("그림나라 화방 혼잡도")
+                menuInflater.inflate(R.menu.popup_menu_hwabang, menu)
+            }
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.select1_hwabang -> joye_button1.setBackgroundResource(R.drawable.button_background1)
+            R.id.select2_hwabang -> joye_button1.setBackgroundResource(R.drawable.button_background2)
+            R.id.select3_hwabang -> joye_button1.setBackgroundResource(R.drawable.button_background3)
+        }
+        return super.onContextItemSelected(item)
     }
 }
+
+
+
+
