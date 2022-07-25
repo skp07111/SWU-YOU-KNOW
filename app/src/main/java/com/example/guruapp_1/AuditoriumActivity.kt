1e26ae309b7be70bd2418e4a2ce6a8a675c7ac73
skp07111 <skp07111@naver.com>
2022-07-25 17:28:10 +0900

    Revert "."
    This reverts commit 8cc526d7f64f54d8bd325ae45069695253b6fd23.

--- app/src/main/java/com/example/guruapp_1/AuditoriumActivity.kt
+++ app/src/main/java/com/example/guruapp_1/AuditoriumActivity.kt
@@ -2,49 +2,10 @@ package com.example.guruapp_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
-import kotlinx.android.synthetic.main.activity_auditorium.*
 
 class AuditoriumActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auditorium)
-        registerForContextMenu(aud_button1)
-        registerForContextMenu(aud_button2)
-    }
-
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-
-        when (v?.id) {
-            R.id.aud_button1 -> {
-                menu?.setHeaderTitle("교직원식당 혼잡도")
-                menuInflater.inflate(R.menu.popup_menu_bookstore, menu)
-            }
-            R.id.aud_button2 -> {
-                menu?.setHeaderTitle("뚜레쥬르 혼잡도")
-                menuInflater.inflate(R.menu.popup_menu_pandorothy, menu)
-            }
-        }
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item?.itemId) {
-            R.id.select1_bookstore -> aud_button1.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_bookstore -> aud_button1.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_bookstore -> aud_button1.setBackgroundResource(R.drawable.button_background3)
-            R.id.select1_pandorothy -> aud_button2.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_pandorothy -> aud_button2.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_pandorothy -> aud_button2.setBackgroundResource(R.drawable.button_background3)
-        }
-        return super.onContextItemSelected(item)
     }
 }
-
-
--- app/src/main/java/com/example/guruapp_1/LibraryCafeActivity.kt
+++ app/src/main/java/com/example/guruapp_1/LibraryCafeActivity.kt
@@ -2,43 +2,10 @@ package com.example.guruapp_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
-import kotlinx.android.synthetic.main.activity_librarycafe.*
 
 class LibraryCafeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_librarycafe)
-        registerForContextMenu(libcafe_button1)
-    }
-
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-
-        when (v?.id) {
-            R.id.libcafe_button1 -> {
-                menu?.setHeaderTitle("북카페 그라찌에 혼잡도")
-                menuInflater.inflate(R.menu.popup_menu_libcafe, menu)
-            }
-        }
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item?.itemId) {
-            R.id.select1_libcafe -> libcafe_button1.setBackgroundResource(R.drawable.button_background1)
-            R.id.select2_libcafe -> libcafe_button1.setBackgroundResource(R.drawable.button_background2)
-            R.id.select3_libcafe -> libcafe_button1.setBackgroundResource(R.drawable.button_background3)
-        }
-        return super.onContextItemSelected(item)
     }
 }
-
-
-
-
