739689696200e34c0dc2f63211aa1c78ebb9d829
skp07111 <skp07111@naver.com>
2022-07-25 18:14:28 +0900

    test

---
+++ app/src/main/java/com/example/guruapp_1/MainMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guruapp_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
