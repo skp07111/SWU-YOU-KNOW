46d04ebac747e86b210aa1c20520fe6ca83ffd94
k-r-1 <juwon_0715@naver.com>
2022-07-27 01:17:29 +0900

    메인 화면&셔틀버스 intent 연결

--- app/src/main/java/com/example/guruapp_1/MainActivity.kt
+++ app/src/main/java/com/example/guruapp_1/MainActivity.kt
@@ -8,14 +8,23 @@ import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var storeMain: Button
+    lateinit var ShuttleBusMain : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         storeMain = findViewById<Button>(R.id.swu_store)
+        ShuttleBusMain = findViewById<Button>(R.id.swu_schoolbus)
 
         storeMain.setOnClickListener() {
             var intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
         }
+
+        ShuttleBusMain.setOnClickListener() {
+            var intent = Intent(this, ShuttleBusMainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
